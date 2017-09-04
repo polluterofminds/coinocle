a959ffff93c354661f21222c83952649ac394e28
Adam Gutierrez <hevyw8@gmail.com>
2017-09-04 15:34:30 -0500

    corrected mongoURI reference, begun local build info in readme

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 const keys = require('./config/keys');
 
-mongoose.connect('keys.mongoURI');
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
