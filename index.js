7a60abb95c9fdefc3cee7581b78a8c295ffe2fd7
Adam Gutierrez <hevyw8@gmail.com>
2017-09-21 17:18:40 -0500

    fixed mongoclient deprecation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const bodyParser = require("body-parser");
-const keys = require("./config/keys");
-require("./models/User");
-require("./services/passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
 
@@ -23,8 +23,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authroutes")(app);
-require("./routes/billingRoute")(app);
+require('./routes/authroutes')(app);
+require('./routes/billingRoute')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
