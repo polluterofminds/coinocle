1bb73144a64cb2c517f31898f7aaaa1e1138bb60
jhunter5811 <jhunter@learningmachine.com>
2017-09-23 19:12:23 -0500

    Designed add wallet form

--- client/src/components/NewWallet.js
+++ client/src/components/NewWallet.js
@@ -6,7 +6,25 @@ class NewWallet extends Component {
   render() {
     return (
         <div>
-          <h1 className="text-center">Add a wallet & starting balance</h1>
+          <div className="addWallet">
+            <h1 className="text-center">Add a wallet & starting balance</h1>
+            <div className="text-center">
+              <form>
+                <p>Enter wallet name</p>
+                <input className="wallet-input" type="text" name="Wallet name" placeholder="Wallet name" />
+                <p>Enter coin type</p>
+                <input type="text" className="wallet-input" name="Coin type" placeholder="Coin type" />
+                <p>Enter starting coin balance</p>
+                <input className="wallet-input" type="text" name="Coins" placeholder="Total coins owned" />
+                <p><input type="submit" value="Save" className="wallet-save" /></p>
+              </form>
+            </div>
+          </div>
+          <div className="addTransactionButton">
+            <a href="#" className="text-center plus-sign">
+              +
+            </a>
+          </div>
         </div>
     );
   }
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //keys.js
 if (process.env.NODE_ENV === "production") {
   //in prod
@@ -6,10 +5,4 @@ if (process.env.NODE_ENV === "production") {
 } else {
   //return dev keys
   module.exports = require("./dev");
-=======
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
-} else {
-  module.exports = require('./dev');
->>>>>>> 3d4cd29ade607029f35501c4e58195a8b71f13ac
 }
