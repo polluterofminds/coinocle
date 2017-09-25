fab5866f1442166040e2f4060545652d48ad8ba8
jhunter5811 <jhunter@learningmachine.com>
2017-09-25 16:25:38 -0500

    fixed path

--- services/passport.js
+++ services/passport.js
@@ -23,14 +23,14 @@ passport.use(
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id });
+      const existingUser = await User.findOne({ googleId: profile.id });
 
-        if (existingUser) {
-          return done(null, existingUser);
-        }
-
-        const user = await new User({ googleId: profile.id }).save();
-        done(null, user);
+      if (existingUser) {
+        return done(null, existingUser);
       }
-    )
-  );
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
