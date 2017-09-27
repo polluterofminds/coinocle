a9af87fb742f547e7ed289db693a7174430310d4
jhunter5811 <jhunter@learningmachine.com>
2017-09-27 14:52:33 -0500

    One more try

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
   twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
   facebookClientID: process.env.FACEBOOK_APP_ID,
