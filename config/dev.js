cd34ad9d9924d598469e06e7539817f223bd15ac
polluterofminds <justin@graphitedocs.com>
2020-03-31 15:59:49 -0500

    migrated to public repo

--- config/dev.js
+++ config/dev.js
@@ -1,22 +1,16 @@
 //dev.js don't commit
 
 module.exports = {
-  googleClientID: "561291313881-d8r1li9ej0tcj6fds2avqk8em2bk96e4.apps.googleusercontent.com",
-  googleClientSecret: "zmufiBSx6_JiNTJRccVkk1B3",
-  mongoURI: "mongodb://justin:password@ds129004.mlab.com:29004/coinocle-dev",
-  cookieKey: "fhasfuqnhjfaksdhfkjhi",
-  stripePublishableKey: "pk_test_JHUXRho7yjfn4sJDmC37uscX",
-  stripeSecretKey: "sk_test_PzNcB6J1xpVQkhshSFVV50WP",
-  twitterConsumerKey: "MYd5lSxqDKxGOcDUCmBelEnvA",
-  twitterConsumerSecret: "X460HTe2T2FFNmnISSFFC1d7tlXrI3my53bmGsFmVCXsBiZnsj",
-  pusherAppId: "419052",
-  pusherKey: "83605a180a043fda21af",
-  pusherClientSecret: "5fbbee35a25670fae939",
-  pusherCluster: "us2"
-  // githubClientID: "dabea3a38fa1330372e9",
-  // githubClientSecret: "ac8b2775dce1e5c530e80577ad79d9ae2db477ee",
-  // facebookClientID: "108518623233187",
-  // facebookClientSecret: "4abe479410aadd5f077a2ad2b419a81d",
-  // coinbaseClientID: "5e10c95430a34c3490a4fd68fd1f40c4fe1f05db351318bfd0df5ea7b06ae29f",
-  // coinbaseClientSecret: "62060e56af300eb4d9bd59a27c61a7994dc4e7dc0f8782adc6b95848baf0ec43"
+  googleClientID: "",
+  googleClientSecret: "",
+  mongoURI: "",
+  cookieKey: "",
+  stripePublishableKey: "",
+  stripeSecretKey: "",
+  twitterConsumerKey: "",
+  twitterConsumerSecret: "",
+  pusherAppId: "",
+  pusherKey: "",
+  pusherClientSecret: "",
+  pusherCluster: ""
 };
